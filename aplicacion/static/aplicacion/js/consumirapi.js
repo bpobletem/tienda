1bf953fd3e5c08b28dccc6b49cf94aa45a46b0ad
Barbara Poblete <bpobletem96@gmail.com>
2024-07-02 16:09:06 -0400

    api funcionando

--- aplicacion/static/aplicacion/js/consumirapi.js
+++ aplicacion/static/aplicacion/js/consumirapi.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-
     // Manejo de la petición a la API de Mindicador
     $.getJSON('https://mindicador.cl/api', function (data) {
-        // Éxito en la obtención de datos
-        console.log(data);
 
         // Acceder al precio del producto desde el DOM
-        const precio = parseFloat($("#precio-producto"));
-        
+        const precioElemento = $("#precio-producto");
+        const precioTexto = precioElemento.text().replace('$', '').replace(',', '').trim();
+        const precio = parseFloat(precioTexto);
 
         // Cálculo del precio en dólares
         if (!isNaN(precio) && data.dolar) {
@@ -21,7 +19,5 @@ $(document).ready(function () {
         // Error al consumir la API
         console.log('Error al consumir la API');
         $("#dolar").text('Error al consumir la API');
-        $("#dolar-actual").text('Error al consumir la API');
     });
-
 });
